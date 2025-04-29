6391d92d9c37b6bd27836d00253551d97b02d2cf
Ivan P <ivan@noshdelivery.co>
2025-04-28 20:02:34 -0600

    Menu view

---
+++ packages/local-cart/src/views/MerchantView.tsx
@@ -0,0 +1,125 @@
+import { AtpAgent } from '@atproto/api'
+import {
+  XyzNoshdeliveryV0CatalogCatalog,
+  XyzNoshdeliveryV0CatalogCollection,
+  XyzNoshdeliveryV0CatalogItem,
+  ComAtprotoRepoListRecords,
+} from '@nosh/lexicon'
+import { useState, useEffect } from 'react'
+import { AtUri } from '@atproto/api'
+
+const REPO = 'did:plc:ufa7rl6agtfdqje6bant3wsb'
+const pdsClient = new AtpAgent({
+  service: `http://localhost:3001`,
+})
+
+async function listRecords<T>(collection: string) {
+  const response = await pdsClient.com.atproto.repo.listRecords({
+    repo: REPO,
+    collection,
+  })
+  return response.data.records as (ComAtprotoRepoListRecords.Record & { value: T })[]
+}
+
+function listToMap<T extends { uri: string }>(list: T[]) {
+  return list.reduce((acc: Record<string, T>, item) => {
+    const uri = new AtUri(item.uri)
+    acc[uri.rkey] = item
+    return acc
+  }, {})
+}
+
+type CatalogRecord = ComAtprotoRepoListRecords.Record & { value: XyzNoshdeliveryV0CatalogCatalog.Record }
+type CollectionRecord = ComAtprotoRepoListRecords.Record & { value: XyzNoshdeliveryV0CatalogCollection.Record }
+type ItemRecord = ComAtprotoRepoListRecords.Record & { value: XyzNoshdeliveryV0CatalogItem.Record }
+
+async function fetchCatalog() {
+  const catalogResponse = await listRecords<XyzNoshdeliveryV0CatalogCatalog.Record>(
+    'xyz.noshdelivery.v0.catalog.catalog',
+  )
+  const collectionResponse = await listRecords<XyzNoshdeliveryV0CatalogCollection.Record>(
+    'xyz.noshdelivery.v0.catalog.collection',
+  )
+  const itemResponse = await listRecords<XyzNoshdeliveryV0CatalogItem.Record>('xyz.noshdelivery.v0.catalog.item')
+  return {
+    catalog: listToMap(catalogResponse),
+    collections: listToMap(collectionResponse),
+    items: listToMap(itemResponse),
+  }
+}
+
+function useCatalog() {
+  const [catalog, setCatalog] = useState<Record<string, CatalogRecord>>({})
+  const [collections, setCollections] = useState<Record<string, CollectionRecord>>({})
+  const [items, setItems] = useState<Record<string, ItemRecord>>({})
+  useEffect(() => {
+    fetchCatalog().then(({ catalog, collections, items }) => {
+      setCatalog(catalog)
+      setCollections(collections)
+      setItems(items)
+    })
+  }, [])
+  return { catalog, collections, items }
+}
+
+function Collection({ collection, items }: { collection: CollectionRecord; items: Record<string, ItemRecord> }) {
+  return (
+    <div>
+      {collection.value.name} - {collection.uri}
+      <ul>
+        {collection.value.items?.map((itemId) => {
+          const item = items[itemId]
+          if (!item) {
+            return `Missing item ${itemId}`
+          }
+          return <Item key={itemId} item={item} />
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function Item({ item }: { item: ItemRecord }) {
+  return (
+    <div>
+      {item.value.name} - {item.uri}: ${item.value.priceMoney.amount}
+    </div>
+  )
+}
+
+function Catalog({
+  catalog,
+  collections,
+  items,
+}: {
+  catalog: CatalogRecord
+  collections: Record<string, CollectionRecord>
+  items: Record<string, ItemRecord>
+}) {
+  return (
+    <div>
+      {catalog.value.name} - {catalog.uri}
+      <ul>
+        {catalog.value.collections?.map((collectionId) => (
+          <Collection key={collectionId} collection={collections[collectionId]} items={items} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function MerchantView() {
+  const { catalog, collections, items } = useCatalog()
+  return (
+    <>
+      <h3>Catalogs</h3>
+      <ul>
+        {Object.values(catalog).map((record) => (
+          <Catalog key={record.uri} catalog={record} collections={collections} items={items} />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default MerchantView
