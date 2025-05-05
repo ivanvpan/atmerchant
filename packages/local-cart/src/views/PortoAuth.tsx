1ab047ed7c1536b45c1f0708ed85a168a4430e03
Ivan P <ivan@noshdelivery.co>
2025-05-05 10:11:59 -0600

    events and stuff

--- packages/local-cart/src/views/PortoAuth.tsx
+++ packages/local-cart/src/views/PortoAuth.tsx
@@ -1,9 +1,11 @@
 import { Hooks } from 'porto/wagmi'
-import { odysseyTestnet, baseSepolia } from 'wagmi/chains'
+import { baseSepolia } from 'wagmi/chains'
 import { porto } from 'porto/wagmi'
 import { useAccount, useConnectors } from 'wagmi'
-import { Hex, Value } from 'ox'
+import { Hex, Json, Value } from 'ox'
 import { http, createConfig, createStorage } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { Porto } from 'porto'
 
 export const wagmiConfig = createConfig({
   chains: [baseSepolia],
@@ -42,7 +44,6 @@ export const permissions = () =>
 
 export default function PortoAuth() {
   const label = 'ivan-account-001'
-  const account = useAccount()
   const connectors = useConnectors()
   const connector = connectors.find((x) => x.id === 'xyz.ithaca.porto')
   const connect = Hooks.useConnect()
@@ -63,7 +64,78 @@ export default function PortoAuth() {
         Register
       </button>
       <p>{connect.error?.message}</p>
-      {account && <p>Account: {account.address}</p>}
+      <Account />
+      <Events />
+    </div>
+  )
+}
+
+function Account() {
+  const account = useAccount()
+  const disconnect = Hooks.useDisconnect()
+
+  return (
+    <div>
+      <h2>Account</h2>
+
+      <div>
+        account: {account.address}
+        <br />
+        chainId: {account.chainId}
+        <br />
+        status: {account.status}
+      </div>
+
+      {account.status !== 'disconnected' && (
+        <button onClick={() => disconnect.mutate({})} type="button">
+          Disconnect
+        </button>
+      )}
+    </div>
+  )
+}
+
+function Events() {
+  const [responses, setResponses] = useState<Record<string, unknown>>({})
+  const connectors = useConnectors()
+  const portoConnector = connectors.find((x) => x.id === 'xyz.ithaca.porto')
+  useEffect(() => {
+    portoConnector
+      ?.getProvider({
+        chainId: baseSepolia.id,
+      })
+      .then((provider: unknown) => {
+        const castedProvider = provider as ReturnType<typeof Porto.create>['provider']
+        const handleResponse = (event: string) => (response: unknown) =>
+          setResponses((responses) => ({
+            ...responses,
+            [event]: response,
+          }))
+
+        const handleAccountsChanged = handleResponse('accountsChanged')
+        const handleChainChanged = handleResponse('chainChanged')
+        const handleConnect = handleResponse('connect')
+        const handleDisconnect = handleResponse('disconnect')
+        const handleMessage = handleResponse('message')
+
+        castedProvider?.on('accountsChanged', handleAccountsChanged)
+        castedProvider?.on('chainChanged', handleChainChanged)
+        castedProvider?.on('connect', handleConnect)
+        castedProvider?.on('disconnect', handleDisconnect)
+        castedProvider?.on('message', handleMessage)
+        return () => {
+          castedProvider?.removeListener('accountsChanged', handleAccountsChanged)
+          castedProvider?.removeListener('chainChanged', handleChainChanged)
+          castedProvider?.removeListener('connect', handleConnect)
+          castedProvider?.removeListener('disconnect', handleDisconnect)
+          castedProvider?.removeListener('message', handleMessage)
+        }
+      })
+  }, [])
+  return (
+    <div>
+      <h3>Events</h3>
+      <pre>{Json.stringify(responses, null, 2)}</pre>
     </div>
   )
 }
